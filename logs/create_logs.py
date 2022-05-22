8bd46575fcf6d22b6a97b2150f7413ba9de00a35
Mikhail Zhuravlev <sestelle@internet.ru>
2022-05-22 16:43:26 +0300

    Add random range in creating logs

--- logs/create_logs.py
+++ logs/create_logs.py
@@ -2,7 +2,7 @@ import gzip
 import os
 import random
 
-from config import LOGS_DATE_START, LOGS_DATE_END
+from config import LOGS_DATE_START, LOGS_DATE_END, LOGS_AMOUNT_MIN, LOGS_AMOUNT_MAX
 from datetime import datetime as dt, timedelta
 
 
@@ -16,7 +16,7 @@ def create_logs():
     while start_date <= end_date:
         file_name = "smbp_" + dt.strftime(start_date, "%d%m%Y") + "_034106_app_01.log"
         file = open(f"log/{file_name}", "w+")
-        for _ in range(500):
+        for _ in range(random.randint(LOGS_AMOUNT_MIN, LOGS_AMOUNT_MAX)):
             if random.randint(1, 2) == 1:
                 file.write("Error some string\n")
             else:
