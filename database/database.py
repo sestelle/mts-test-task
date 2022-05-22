14553960bdc3a2d37ed3af984a6b9593ae2898ec
Mikhail Zhuravlev <sestelle@internet.ru>
2022-05-22 17:57:06 +0300

    Add check DATABASE_URL in config.py

--- database/database.py
+++ database/database.py
@@ -2,7 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from config import DATABASE_URL
+
+import sys
+
 import config as config
+
+if not DATABASE_URL:
+    print("Variable DATABASE_URL in config.py is empty")
+    sys.exit()
 
 engine = create_engine(
     config.DATABASE_URL
